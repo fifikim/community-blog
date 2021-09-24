83dd618ec41725107334312fb2efa310b872b076
Sophia Kim <mail@fifikim.com>
2021-09-24 14:19:05 -0400

    initial commit

---
+++ src/user/PostList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import Post from "./Post";
+import PostLink from "./PostLink";
+import NoPostSelectedMessage from "./NoPostSelectedMessage";
+
+export const PostList = ({ posts }) => {
+  const { url } = useRouteMatch();
+  const postLinks = posts.map((post) => (
+    <PostLink key={post.id} userId={post.userId} post={post} />
+  ));
+
+  return (
+    <div className="row pt-2">
+      <div className="col-3">
+        <ul className="list-group">{postLinks}</ul>
+      </div>
+      <div className="col-9">
+        <Switch>
+          <Route path={`${url}/:postId`}>
+            <Post posts={posts} />
+          </Route>
+          <Route exact path={url}>
+            <NoPostSelectedMessage />
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  );
+};
+
+export default PostList;
